a26d861769002bb0b25171d0faeb847001ec0dd5
Tiny Company <talktotinytales@gmail.com>
2026-07-12 12:46:13 -0300

    Reenviando arquivos

---
+++ temp.js
@@ -0,0 +1,6 @@
+
+            setTimeout(() => {
+                const overlay = document.getElementById('timeout-overlay');
+                if (overlay) overlay.style.display = 'flex';
+            }, 50 * 60 * 1000); // 50 minutos
+        
